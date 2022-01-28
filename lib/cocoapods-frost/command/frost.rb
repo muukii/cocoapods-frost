1bd3dc7313cc9eb2a21999f5140d3e251e261040
Muukii <muukii.app@gmail.com>
2022-01-29 03:33:18 +0900

    :evergreen_tree: Update

--- lib/cocoapods-frost/command/frost.rb
+++ lib/cocoapods-frost/command/frost.rb
@@ -22,7 +22,21 @@ module Pod
 
       def install
 
-        p config
+        p config.sandbox_root
+
+        sandbox = Sandbox.new(File.join(config.sandbox_root, "Frost"))
+        
+        # podfile = Pod::Podfile.new(File.join(config.sandbox_root, "../Podfile"))
+        podfile = Pod::Podfile.from_file(File.join(config.sandbox_root, "../Podfile"))
+
+        installer = Installer.new(sandbox, podfile)
+
+        installer.repo_update = false
+        installer.podfile.installation_options.integrate_targets = false
+        installer.podfile.installation_options.warn_for_multiple_pod_sources = false
+        installer.podfile.installation_options.deterministic_uuids = false
+
+        installer.install!
       end
 
     end
